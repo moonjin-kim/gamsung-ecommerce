e1f94dc9ca2f0875a7438a38cc5c29f3b60d1420
moonjin-kim <ace39418@gmail.com>
2025-07-13 16:58:44 +0900

    feat: 회원가입 api 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/member/UserRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.loopers.infrastructure.member;
+
+import com.loopers.domain.user.User;
+import com.loopers.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class UserRepositoryImpl implements UserRepository {
+    private final UserJpaRepository userJpaRepository;
+
+    @Override
+    public User save(User user) {
+        return userJpaRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> find(Long id) {
+        return userJpaRepository.findById(id);
+    }
+
+    @Override
+    public Optional<User> findByAccount(String account) {
+        return userJpaRepository.findByAccount(account);
+    }
+}
