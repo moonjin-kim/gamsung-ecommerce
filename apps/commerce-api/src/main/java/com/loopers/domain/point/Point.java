a807febb527c429c26b6f6480ff41f3c5c0b4b7d
moonjin-kim <ace39418@gmail.com>
2025-07-13 21:26:35 +0900

    feat: 포인트 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/Point.java
@@ -0,0 +1,20 @@
+package com.loopers.domain.point;
+
+import com.loopers.domain.BaseEntity;
+import com.loopers.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "point")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Point extends BaseEntity {
+    int amount;
+    PointStatus status;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
