50759c83d021fef9e96b11c23a737fba3bbae246
moonjin-kim <ace39418@gmail.com>
2025-07-13 21:52:45 +0900

    feat: 포인트 Domain 충전 기능 추가

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1ApiSpec.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/user/UserV1ApiSpec.java
@@ -2,7 +2,6 @@ package com.loopers.interfaces.api.user;
 
 import com.loopers.domain.user.UserRegisterRequest;
 import com.loopers.interfaces.api.ApiResponse;
-import com.loopers.interfaces.api.example.ExampleV1Dto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
