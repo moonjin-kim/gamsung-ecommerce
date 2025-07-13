afe8b17102148dc9a8b8c92a493ed93216476155
moonjin-kim <ace39418@gmail.com>
2025-07-13 15:37:26 +0900

    feat: member repository 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/member/MemberRepositoryImpl.java
@@ -0,0 +1,25 @@
+package com.loopers.infrastructure.member;
+
+import com.loopers.domain.member.Member;
+import com.loopers.domain.member.MemberRepository;
+import com.loopers.infrastructure.example.ExampleJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class MemberRepositoryImpl implements MemberRepository {
+    private final MemberJpaRepository memberJpaRepository;
+
+    @Override
+    public Member save(Member member) {
+        return memberJpaRepository.save(member);
+    }
+
+    @Override
+    public Optional<Member> find(Long id) {
+        return memberJpaRepository.findById(id);
+    }
+}
