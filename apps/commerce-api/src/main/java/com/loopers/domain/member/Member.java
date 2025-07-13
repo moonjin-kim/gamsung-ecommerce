0d1dc123613d13d410088637390c24e64f07b49d
moonjin-kim <ace39418@gmail.com>
2025-07-13 15:10:30 +0900

    refactor: email 객체로 분리

--- apps/commerce-api/src/main/java/com/loopers/domain/member/Member.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/member/Member.java
@@ -1,13 +1,17 @@
 package com.loopers.domain.member;
 
 import com.loopers.domain.BaseEntity;
+import com.loopers.domain.Email;
 import com.loopers.support.error.CoreException;
 import com.loopers.support.error.ErrorType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
 
 import java.util.regex.Pattern;
 
@@ -17,21 +21,22 @@ import java.util.regex.Pattern;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
 
+    @Column(length = 100, nullable = false)
+    String account;
+    @Column(length = 100, nullable = false)
     String name;
 
-    String account;
-
-    String email;
-
+    @Embedded
+    Email email;
+    @Column(length = 100, nullable = false)
     String password;
 
     String birthday;
-
+    @Column(length = 100, nullable = false)
     String address;
 
 
     static String ACCOUNT_PATTERN = "^[a-z0-9]{1,10}$";
-    static String EMAIL_PATTERN = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9_+&*-]+\\.)+[a-zA-Z]{2,7}$";
     static String BIRTHDAY_PATTERN = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])$";
 
     static Member create(MemberRegisterRequest registerRequest) {
@@ -44,10 +49,7 @@ public class Member extends BaseEntity {
         member.name = registerRequest.name();
         member.password = registerRequest.password();
 
-        if(!Pattern.matches(EMAIL_PATTERN, registerRequest.email())) {
-            throw new CoreException(ErrorType.BAD_REQUEST, "이메일 형식이 잘못되었습니다.");
-        }
-        member.email = registerRequest.email();
+        member.email = new Email(registerRequest.email());
         if(!Pattern.matches(BIRTHDAY_PATTERN, registerRequest.birthday())) {
             throw new CoreException(ErrorType.BAD_REQUEST, "생일 형식이 잘못되었습니다.");
         }
