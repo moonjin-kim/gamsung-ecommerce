7534aaee037e10e99eab94c8f1f480ae587397e6
moonjin-kim <ace39418@gmail.com>
2025-07-13 15:47:22 +0900

    test: 회원가입 테스트 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/member/MemberService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/member/MemberService.java
@@ -2,8 +2,16 @@ package com.loopers.domain.member;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Component
+@Transactional(readOnly = true)
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member registerMember(MemberRegisterRequest request) {
+
+        return new Member();
+    }
 }
