2c9a0ba1ed96addaf0898cc330489545c455a866
moonjin-kim <ace39418@gmail.com>
2025-07-13 15:37:26 +0900

    feat: member repository 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/member/MemberService.java
@@ -0,0 +1,9 @@
+package com.loopers.domain.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class MemberService {
+}
