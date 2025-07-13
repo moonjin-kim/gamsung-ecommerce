2e56ff81ddbc1e40e6c039b4370f8d74ef210a58
moonjin-kim <ace39418@gmail.com>
2025-07-13 17:35:01 +0900

    modify: 불필요한 user 필드 제거

--- apps/commerce-api/src/main/java/com/loopers/application/user/UserInfo.java
+++ apps/commerce-api/src/main/java/com/loopers/application/user/UserInfo.java
@@ -8,20 +8,16 @@ import java.time.LocalDate;
 public record UserInfo(
         Long id,
         String account,
-        String name,
         String email,
         LocalDate birthday,
-        String address,
         Sex sex
 ) {
     public static UserInfo from(User user) {
         return new UserInfo(
                 user.getId(),
                 user.getAccount(),
-                user.getName(),
-                user.getEmail().address(),
+                user.getEmail(),
                 user.getBirthday(),
-                user.getAddress(),
                 user.getSex()
         );
     }
--- apps/commerce-api/src/test/java/com/loopers/interfaces/api/user/UserV1ApiE2ETest.java
+++ apps/commerce-api/src/test/java/com/loopers/interfaces/api/user/UserV1ApiE2ETest.java
@@ -1,14 +1,30 @@
 package com.loopers.interfaces.api.user;
 
+import com.loopers.domain.example.ExampleModel;
+import com.loopers.domain.user.Sex;
+import com.loopers.domain.user.UserRegisterRequest;
+import com.loopers.interfaces.api.ApiResponse;
+import com.loopers.interfaces.api.example.ExampleV1Dto;
 import com.loopers.utils.DatabaseCleanUp;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserV1ApiE2ETest {
-    private static final String ENDPOINT_REGISTER = "/api/v1/examples/";
+    private static final String ENDPOINT_REGISTER = "/api/v1/users";
 
     private final TestRestTemplate testRestTemplate;
     private final DatabaseCleanUp databaseCleanUp;
@@ -25,5 +41,31 @@ class UserV1ApiE2ETest {
     @AfterEach
     void tearDown() {
         databaseCleanUp.truncateAllTables();
+    }
+
+    @DisplayName("Post /api/v1/users")
+    @Nested
+    class RegisterUser {
+        @DisplayName("")
+        @Test
+        void returnsUserInfo_whenValidBodyIsProvided() {
+            //given
+            UserRegisterRequest request = new UserRegisterRequest(
+                    "gil123","gildong@gmail.com", "2020-01-01", Sex.MALE
+            );
+
+            //when
+            ParameterizedTypeReference<ApiResponse<UserV1Dto.UserResponse>> responseType = new ParameterizedTypeReference<>() {
+            };
+            ResponseEntity<ApiResponse<UserV1Dto.UserResponse>> response =
+                    testRestTemplate.exchange(ENDPOINT_REGISTER, HttpMethod.POST, new HttpEntity<UserRegisterRequest>(request), responseType);
+
+            //then
+            assertAll(
+                    () -> assertTrue(response.getStatusCode().is2xxSuccessful()),
+                    () -> assertThat(response.getBody().data().id()).isNotNull(),
+                    () -> assertThat(response.getBody().data().account()).isEqualTo(request.account())
+            );
+        }
     }
 }
