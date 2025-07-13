cc9785de5c71c2568de31decb53f8560cd1462e0
moonjin-kim <ace39418@gmail.com>
2025-07-13 14:45:28 +0900

    test: 회원가입 테스트 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/member/Member.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/member/Member.java
@@ -1,13 +1,19 @@
 package com.loopers.domain.member;
 
 import com.loopers.domain.BaseEntity;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.regex.Pattern;
 
 @Entity
 @Table(name = "member")
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
 
@@ -15,9 +21,27 @@ public class Member extends BaseEntity {
 
     String account;
 
+    String email;
+
     String password;
 
     String birthday;
 
     String address;
+
+
+    static Member create(MemberRegisterRequest registerRequest) {
+        Member member = new Member();
+
+        member.account = registerRequest.account();
+        member.name = registerRequest.name();
+        member.password = registerRequest.password();
+        member.email = registerRequest.email();
+        member.birthday = registerRequest.birthday();
+        member.address = registerRequest.address();
+
+        return member;
+    }
+
+
 }
