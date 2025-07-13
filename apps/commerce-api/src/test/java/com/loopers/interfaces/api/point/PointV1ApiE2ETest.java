3af0836e028a75102a61adf985689d3b66441585
moonjin-kim <ace39418@gmail.com>
2025-07-13 23:33:15 +0900

    test: 유저 포인트 잔액 조회 api E2E 테스트 추가

--- apps/commerce-api/src/test/java/com/loopers/interfaces/api/point/PointV1ApiE2ETest.java
+++ apps/commerce-api/src/test/java/com/loopers/interfaces/api/point/PointV1ApiE2ETest.java
@@ -25,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PointV1ApiE2ETest {
     private static final String ENDPOINT_CHARGE = "/api/v1/points/charge";
+    private static final String ENDPOINT_GET_BALANCE = "/api/v1/points";
 
     private final UserJpaRepository userJpaRepository;
     private final PointJpaRepository pointJpaRepository;
@@ -132,6 +133,98 @@ public class PointV1ApiE2ETest {
                             ENDPOINT_CHARGE,
                             HttpMethod.POST,
                             new HttpEntity<PointChargeRequest>(request, headers),
+                            responseType
+                    );
+
+            //then
+            assertAll(
+                    () -> assertTrue(response.getStatusCode().is4xxClientError()),
+                    () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND)
+            );
+        }
+    }
+
+    @DisplayName("GET /api/v1/points")
+    @Nested
+    class GetBalance {
+        @DisplayName("내 잔액 조회에 성공할 경우, 충전된 보유 총량을 응답으로 반환한다.")
+        @Test
+        void returnsUserBalance_whenValidUserIdIsProvided() {
+            //given
+            User user = userJpaRepository.save(
+                    UserFixture.createMember()
+            );
+            Point point = pointJpaRepository.save(
+                    Point.charge(user, 10000, 0)
+            );
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.set("X-USER-ID", String.valueOf(user.getId()));
+
+            //when
+            ParameterizedTypeReference<ApiResponse<PointV1Dto.PointBalanceResponse>> responseType = new ParameterizedTypeReference<>() {
+            };
+            ResponseEntity<ApiResponse<PointV1Dto.PointBalanceResponse>> response =
+                    testRestTemplate.exchange(
+                            ENDPOINT_GET_BALANCE,
+                            HttpMethod.GET,
+                            new HttpEntity<>(null, headers),
+                            responseType
+                    );
+
+            //then
+            assertAll(
+                    () -> assertTrue(response.getStatusCode().is2xxSuccessful()),
+                    () -> assertThat(response.getBody().data().balance()).isEqualTo(point.getBalance())
+            );
+        }
+
+        @DisplayName("충전 이력이 없는 유저가 잔액 조회에 성공할 경우, 잔액이 0으로 응답이 반환된다")
+        @Test
+        void returnsUserZeroBalance_whenValidUserIdIsProvidedAndNotChargedHistory() {
+            //given
+            User user = userJpaRepository.save(
+                    UserFixture.createMember()
+            );
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.set("X-USER-ID", String.valueOf(user.getId()));
+
+            //when
+            ParameterizedTypeReference<ApiResponse<PointV1Dto.PointBalanceResponse>> responseType = new ParameterizedTypeReference<>() {
+            };
+            ResponseEntity<ApiResponse<PointV1Dto.PointBalanceResponse>> response =
+                    testRestTemplate.exchange(
+                            ENDPOINT_GET_BALANCE,
+                            HttpMethod.GET,
+                            new HttpEntity<>(null, headers),
+                            responseType
+                    );
+
+            //then
+            assertAll(
+                    () -> assertTrue(response.getStatusCode().is2xxSuccessful()),
+                    () -> assertThat(response.getBody().data().balance()).isEqualTo(0)
+            );
+        }
+
+        @DisplayName("존재하지 않는 유저로 요청할 경우, 404 Not Found 응답을 반환한다.")
+        @Test
+        void throwsException_whenInvalidIdIsProvided() {
+            //given
+            PointChargeRequest request = new PointChargeRequest(1000);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.set("X-USER-ID", "1");
+
+            //when
+            ParameterizedTypeReference<ApiResponse<PointV1Dto.PointBalanceResponse>> responseType = new ParameterizedTypeReference<>() {
+            };
+            ResponseEntity<ApiResponse<PointV1Dto.PointBalanceResponse>> response =
+                    testRestTemplate.exchange(
+                            ENDPOINT_GET_BALANCE,
+                            HttpMethod.GET,
+                            new HttpEntity<>(null, headers),
                             responseType
                     );
 
