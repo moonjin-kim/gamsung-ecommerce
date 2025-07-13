7b30488cc5f3dcdc614a3dd46a13cc407fa8325f
moonjin-kim <ace39418@gmail.com>
2025-07-13 16:49:22 +0900

    refactor: member -> user

--- apps/commerce-api/src/main/java/com/loopers/domain/member/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.loopers.domain.member;
-
-import java.util.Optional;
-
-public interface MemberRepository {
-    Member save(Member member);
-    Optional<Member> find(Long id);
-    Optional<Member> findByAccount(String account);
-}
