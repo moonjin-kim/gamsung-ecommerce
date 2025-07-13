af37224bb6440c940d8007fc117f70f40431b489
moonjin-kim <ace39418@gmail.com>
2025-07-13 16:49:22 +0900

    refactor: member -> user

--- apps/commerce-api/src/main/java/com/loopers/domain/member/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.loopers.domain.member;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-@RequiredArgsConstructor
-@Component
-public class MemberService {
-}
