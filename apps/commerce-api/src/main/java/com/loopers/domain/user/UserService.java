8fef6af4cd74abbc630bc17bd8f6018020a3f173
moonjin-kim <ace39418@gmail.com>
2025-07-13 20:17:28 +0900

    test: 회원 조회 테스트 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/user/UserService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserService.java
@@ -19,8 +19,12 @@ public class UserService {
         return memberRepository.save(User.create(request));
     }
 
+    @Transactional(readOnly = true)
+    public User getUser(Long id) {
+        return new User();
+    }
+
     private void checkDuplicateAccount(UserRegisterRequest request) {
-        Boolean isAlready = memberRepository.findByAccount(request.account()).isPresent();
         if (memberRepository.findByAccount(request.account()).isPresent()) {
             throw new CoreException(ErrorType.BAD_REQUEST,"이미 존재하는 아이디입니다: " + request.account());
         }
