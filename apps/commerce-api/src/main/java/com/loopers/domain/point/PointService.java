b2df8c17ee7b478d48c2033da59f192fd918d94f
moonjin-kim <ace39418@gmail.com>
2025-07-13 22:23:21 +0900

    feat: 포인트 충전 기능 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/point/PointService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/PointService.java
@@ -2,9 +2,13 @@ package com.loopers.domain.point;
 
 import com.loopers.domain.user.User;
 import com.loopers.domain.user.UserRepository;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Component
@@ -14,10 +18,13 @@ public class PointService {
     private final UserRepository userRepository;
 
     public Point chargePoint(Long userId, int amount) {
-        User user = userRepository.find(userId).orElseThrow();
+        User user = userRepository.find(userId).orElseThrow(() ->
+                new CoreException(ErrorType.NOT_FOUND, "[id = " + userId + "] 존재하지 않는 회원입니다.")
+        );
 
-        Point point = Point.charge(user, amount, 0);
+        Optional<Point> lastPoint = pointRepository.findLastByUser(user);
+        int currentBalance = lastPoint.map(Point::getBalance).orElse(0);
 
-        return point;
+        return Point.charge(user, amount, currentBalance);
     }
 }
