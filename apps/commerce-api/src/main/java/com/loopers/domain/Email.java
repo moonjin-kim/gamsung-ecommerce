7bc66397ee4878cb8096130355121f2f39677cfb
moonjin-kim <ace39418@gmail.com>
2025-07-13 15:10:30 +0900

    refactor: email 객체로 분리

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/Email.java
@@ -0,0 +1,18 @@
+package com.loopers.domain;
+
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.util.regex.Pattern;
+
+@Embeddable
+public record Email(@Column(name = "email", length = 150, nullable = false)String address) {
+    static String EMAIL_PATTERN = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9_+&*-]+\\.)+[a-zA-Z]{2,7}$";
+    public Email {
+        if(!Pattern.matches(EMAIL_PATTERN, address)) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "이메일 형식이 잘못되었습니다.");
+        }
+    }
+}
