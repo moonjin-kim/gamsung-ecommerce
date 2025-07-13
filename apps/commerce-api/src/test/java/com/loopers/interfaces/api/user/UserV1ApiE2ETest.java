9cdd786662e071170aea154fb5d809402f26a1c0
moonjin-kim <ace39418@gmail.com>
2025-07-13 19:56:29 +0900

    add: register 값 빈 값 여부 확인 기능 추가

--- apps/commerce-api/src/test/java/com/loopers/interfaces/api/user/UserV1ApiE2ETest.java
+++ apps/commerce-api/src/test/java/com/loopers/interfaces/api/user/UserV1ApiE2ETest.java
@@ -10,11 +10,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -72,30 +77,37 @@ class UserV1ApiE2ETest {
             );
         }
 
+        @DisplayName("회원가입 시 잘못된 값이 들어오면, 400 BAD_REQUEST 응답을 받는다.")
+        @ParameterizedTest
+        @MethodSource("provideInvalidUserRegisterRequests")
+        void returnsBadRequest_whenInvalidBodyIsProvided(UserRegisterRequest invalidRequest) {
+            // given
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<UserRegisterRequest> requestEntity = new HttpEntity<>(invalidRequest, headers);
 
-        @DisplayName("성별을 포함하지 않으면, 400 BAD_REQUEST 응답을 받는다.")
-        @Test
-        void throwsBadRequest_whenSexIsNotProvider() {
-            //given
-            UserRegisterRequest request = new UserRegisterRequest(
-                    "gil123","gildong@gmail.com", "2020-01-01", null
+            // when
+            ResponseEntity<Void> response = testRestTemplate.exchange(
+                    ENDPOINT_REGISTER,
+                    HttpMethod.POST,
+                    requestEntity,
+                    Void.class
             );
 
-            //when
-            ParameterizedTypeReference<ApiResponse<UserV1Dto.UserResponse>> responseType = new ParameterizedTypeReference<>() {
-            };
-            ResponseEntity<ApiResponse<UserV1Dto.UserResponse>> response =
-                    testRestTemplate.exchange(
-                            ENDPOINT_REGISTER,
-                            HttpMethod.POST,
-                            new HttpEntity<UserRegisterRequest>(request),
-                            responseType
-                    );
-
-            //then
+            // then
             assertAll(
                     () -> assertTrue(response.getStatusCode().is4xxClientError()),
                     () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST)
+            );
+        }
+
+
+        private static Stream<Arguments> provideInvalidUserRegisterRequests() {
+            return Stream.of(
+                    Arguments.of(new UserRegisterRequest(null, "test@email.com", "2000-01-01", Sex.MALE)), //아이디 없음
+                    Arguments.of(new UserRegisterRequest("short", null, "2000-01-01", Sex.MALE)), // 이메일 없음
+                    Arguments.of(new UserRegisterRequest("hong123", "test@email.com", null, Sex.MALE)), //생년월일 없음
+                    Arguments.of(new UserRegisterRequest("hong1234", "test@email.com", "2000-01-01", null))  //성별없ㅇ므
             );
         }
     }
