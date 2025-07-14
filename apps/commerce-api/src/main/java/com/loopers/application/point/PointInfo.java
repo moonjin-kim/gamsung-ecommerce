cd75bf596a15e833bbda5c0c7afe2bb6191e105f
moonjin-kim <ace39418@gmail.com>
2025-07-14 20:49:09 +0900

    refactor: PointService의 유저 조회 로직 Facade로 이전

--- apps/commerce-api/src/main/java/com/loopers/application/point/PointInfo.java
+++ apps/commerce-api/src/main/java/com/loopers/application/point/PointInfo.java
@@ -1,10 +1,12 @@
 package com.loopers.application.point;
 
 import com.loopers.domain.point.Point;
+import com.loopers.domain.user.User;
 
-public record PointInfo(int balance) {
-    public static PointInfo from(Point point) {
+public record PointInfo(Long userId, int balance) {
+    public static PointInfo from(User user, Point point) {
         return new PointInfo(
+                user.getId(),
                 point.getBalance()
         );
     }
