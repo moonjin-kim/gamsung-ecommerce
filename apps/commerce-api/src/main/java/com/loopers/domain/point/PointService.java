a9d6ce1bbc2abdb55c65845fff961540654b911c
moonjin-kim <ace39418@gmail.com>
2025-07-14 23:57:18 +0900

    test: 회원가입 검증 테스트 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/point/PointService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/PointService.java
@@ -2,19 +2,18 @@ package com.loopers.domain.point;
 
 import com.loopers.domain.user.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @RequiredArgsConstructor
-@Component
+@Service
 @Transactional(readOnly = true)
 public class PointService {
     private final PointRepository pointRepository;
 
     public Point chargePoint(User user, int amount) {
-
         Optional<Point> lastPoint = pointRepository.findLastByUser(user);
         int currentBalance = lastPoint.map(Point::getBalance).orElse(0);
 
